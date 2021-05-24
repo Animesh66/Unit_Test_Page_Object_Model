3774cc9bf3c5dd2a3fc8b6eda1fc1b4b06118156
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-24 20:22:53 +0530

    Login Page completed

--- Page_Object_Model/Pages/login_page.py
+++ Page_Object_Model/Pages/login_page.py
@@ -8,10 +8,15 @@ class LoginPage():
         self.driver = driver
 
     def set_email(self, email):
+        self.driver.find_element_by_id(self.email_textbox_id).clear()
         self.driver.find_element_by_id(self.email_textbox_id).send_keys(email)
 
     def set_password(self, password):
+        self.driver.find_element_by_id(self.password_textbox_id).clear()
         self.driver.find_element_by_id(self.password_textbox_id).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element_by_id(self.password_textbox_id).submit()
 
     def click_logout(self):
         self.driver.find_elemet_by_link_text(self.logout_link_text).click()
--- Page_Object_Model/TestCases/login_test.py
+++ Page_Object_Model/TestCases/login_test.py
@@ -3,7 +3,38 @@ import unittest
 import HtmlTestRunner
 from Page_Object_Model.Pages.login_page import LoginPage
 import sys  # this is imported to et the PATH environment variable value
+import time
 
 # Set PATH environment variable value to current folder location
-sys.path.append("/Users/animeshmukherjee/PycharmProjects/pythonProject/Unit_Test_Page_Object_Model")
+# sys.path.append("/Users/animeshmukherjee/PycharmProjects/pythonProject/Unit_Test_Page_Object_Model")
 
+
+class TestLogin(unittest.TestCase):
+    base_url = "https://admin-demo.nopcommerce.com/"
+    email = "admin@yourstore.com"
+    password = "admin"
+    driver = webdriver.Chrome()
+
+    @classmethod  # defined the setUpClass to execute as first method
+    def setUpClass(cls):
+        cls.driver.get(cls.base_url)
+        cls.driver.maximize_window()
+
+    def test_login(self):
+        login_page = LoginPage(self.driver)
+        login_page.set_email(self.email)
+        login_page.set_password(self.password)
+        time.sleep(3)
+        login_page.click_login()
+        time.sleep(6)
+        self.assertEqual("Dashboard / nopCommerce administration", self.driver.title, "Website title does not match")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='/Users/animeshmukherjee/Desktop/Animesh/Log_file'))
+
+# To run this test case in terminal type "python3 Page_Object_Model/TestCases/login_test.py"
