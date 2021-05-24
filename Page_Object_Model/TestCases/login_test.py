8a607839aeefb971ff8903f81a401b5c8cc94fb9
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-24 17:14:16 +0530

    Login Page completed

--- Page_Object_Model/TestCases/login_test.py
+++ Page_Object_Model/TestCases/login_test.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
 import unittest
 import HtmlTestRunner
+from Page_Object_Model.Pages.login_page import LoginPage
+import sys  # this is imported to et the PATH environment variable value
+
+# Set PATH environment variable value to current folder location
+sys.path.append("/Users/animeshmukherjee/PycharmProjects/pythonProject/Unit_Test_Page_Object_Model")
+
