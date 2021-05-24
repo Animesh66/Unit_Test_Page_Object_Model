4ebf69c2388796ea9e71ca27baffd6762d1b1961
Animesh Mukherjee <er.animesh6@hotmail.com>
2021-05-24 16:58:36 +0530

    Login Page completed

---
+++ Page_Object_Model/Pages/login_page.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+
+class LoginPage():
+    # Define all the locators of the element
+    email_textbox_id = "Email"
+    password_textbox_id = "Password"
+    logout_link_text = "Logout"
+
+    def __init__(self, driver):  # constructor initialized to create the driver
+        self.driver = driver
+
+    def set_email(self, email):
+        self.driver.find_element_by_id(self.email_textbox_id).send_keys(email)
+
+    def set_password(self, password):
+        self.driver.find_element_by_id(self.password_textbox_id).send_keys(password)
+
+    def click_logout(self):
+        self.driver.find_elemet_by_link_text(self.logout_link_text).click()
